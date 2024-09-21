ffa7e5d9be1a9e104a644f01c438052b3fd81afd
MCS\NBO <Nataraj.Boina@spacewell.com>
2024-09-21 17:31:15 +0530

    remove unnecessary text

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,10 @@ import { CompilerBox } from "./compiler-box/compiler.box";
 function App() {
   return (
     <>
-      <h1>Nemetschek Hackathon coding round</h1>
+      <h1>Hackathon coding round</h1>
       <div className="container">
         <CompilerBox defaultLanguage={PROGRAMMING_LANGUAGES.JAVASCRIPT} />
       </div>
-      <p className="foot-notes">©️ Spacewell India (A Nemetschek Company)</p>
     </>
   );
 }
